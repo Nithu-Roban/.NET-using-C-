45cfb1fd3586e2cfa505ac1ba2645291d4c4cc45
Nithu Roban <122724772+Nithu-Roban@users.noreply.github.com>
2023-08-08 11:19:44 +0530

    Add files via upload

---
+++ directory/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace directory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\nithu";
+            Directory.CreateDirectory(path);
+            string[] a = Directory.GetDirectories(path);
+            for(int i=0; i<a.Length;i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
