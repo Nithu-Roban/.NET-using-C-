afe18ea22886d9fb50bf2922d8a759a3804c3f1b
Nithu Roban <122724772+Nithu-Roban@users.noreply.github.com>
2023-08-11 10:40:18 +0530

    Add files via upload

---
+++ CheckBox/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = tb1.Text;
+            string pass = tb2.Text;
+
+            if(name =="nithu" && pass == "123")
+            {
+                Form2  form = new Form2(tb1.Text);
+                form.Show();
+                this.Hide();
+               
+            }
+        }
+    }
+}
---
+++ FactorialApplication/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fact=1, count=0;
+            
+            int num = Convert.ToInt32(tb1.Text);
+         //  label2.Text = num.ToString();
+            for(int i = num; i >=1;i--)
+            {
+                fact =fact* i;
+                count++;
+            }
+            label2.Text = fact.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+     
+        }
+    }
+}
