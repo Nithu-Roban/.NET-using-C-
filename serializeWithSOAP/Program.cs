0ccb0d471283eb7dd5c267776bce5c530394eca6
Nithu Roban <122724772+Nithu-Roban@users.noreply.github.com>
2023-09-08 10:30:18 +0530

    Add files via upload

---
+++ serializeWithSOAP/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+[Serializable]
+public class Student
+{
+    public int rno;
+    public string name;
+    public Student(){ }
+     public Student(int rno, string name)
+     { 
+        this.rno = rno; 
+        this.name = name;
+     }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Student s = new Student(1,"Anna");
+        //Student st = new Student(2, "Nithu");
+        //serializing with SOAP
+        XmlSerializer serializer = new XmlSerializer(typeof(Student));
+        FileStream fs = new FileStream("E:\\nithu\\serialSOAP.txt", FileMode.OpenOrCreate);
+        serializer.Serialize(fs, s);
+        //serializer.Serialize(fs, st);
+        //Deserialize
+        fs.Position = 0;
+        Student s1 = (Student)serializer.Deserialize(fs);
+        Console.WriteLine("Deserialized object");
+      
+            Console.WriteLine($"Name : {s1.name}\n rollno : {s.rno}");
+        
+    }
+}
---
+++ serializingPrgm1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+[Serializable]
+
+    public class Student
+    {
+        public int rollno;
+        public string name;
+        public Student(int rollno, string name)
+        {
+            this.rollno = rollno;
+            this.name = name;
+        }
+
+    }
+    public class SerializeExample
+    {
+        public static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("E:\\nithu\\serialDeserialFile.txt", FileMode.OpenOrCreate);
+            BinaryFormatter bf = new BinaryFormatter();
+            Student s = new Student(101, "Nithu");
+            // serialization
+            bf.Serialize(fs, s);
+            Console.WriteLine("Object is stored in the file");
+
+            //deserializing
+
+            deserializing.disp();
+            fs.Close();
+        }
+    }
+    public class deserializing
+    {
+        public static void disp()
+        {
+            FileStream fs = new FileStream("E:\\nithu\\SerializedFile.txt", FileMode.OpenOrCreate);
+            BinaryFormatter bf = new BinaryFormatter();
+
+            Student S = (Student)bf.Deserialize(fs);
+            Console.WriteLine("RollNo: " + S.rollno);
+            Console.WriteLine("Name: " + S.name);
+        }
+    }
