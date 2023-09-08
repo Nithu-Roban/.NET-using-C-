203e70f1ee3ab2e36cee0418deb5f9713334c40c
Nithu Roban <122724772+Nithu-Roban@users.noreply.github.com>
2023-09-08 10:27:55 +0530

    Add files via upload

---
+++ StringHandling/Program.cs
@@ -0,0 +1,10 @@
+﻿class StringHandlingFunction
+{
+    public static void Main(string[] args)
+    {
+        string fname = "Nithu";
+        string lname = "Roban";
+        string name = $"full name is {fname}{lname}";
+        Console.WriteLine(name);
+    }
+}
