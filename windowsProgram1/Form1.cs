131534a97b500bfcfa47a8dc4a4a3c5eb777005f
Nithu Roban <122724772+Nithu-Roban@users.noreply.github.com>
2023-09-08 10:31:04 +0530

    Add files via upload

---
+++ windowsProgram1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windowsProgram1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String temp;
+            temp = label1.Text;
+            label1.Text = label2.Text;
+            label2.Text = temp;
+            button1.BackColor = Color.Red;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(tb1.Text == "nithu")
+            {
+                button2.BackColor = Color.Purple;
+                label4.Text = "Hiii \t" + tb1.Text;
+            }
+        }
+    }
+}
